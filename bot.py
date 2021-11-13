849d0656ac58ebe77c86c1e55347434cd1f2f7a3
Joe Kennerly <joseph.kennerly@gmail.com>
2021-11-12 18:20:04 -0600

    initial commit

---
+++ bot.py
@@ -0,0 +1,74 @@
+import os
+import re
+import random
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+@bot.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send('You do not have the correct role for this command.')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    hi = re.compile(r'hi')
+    good = re.compile(r'good')
+    
+    is_hi = hi.search(message.content)
+    is_good = good.search(message.content)
+    if is_hi: 
+        await message.channel.send('hi')
+    elif is_good:
+        await message.channel.send('happy')
+
+@bot.command(name='create-channel', help='Creates a new channel')
+@commands.has_role('admin')
+async def create_channel(ctx, channel_name='real-python'):
+    guild = ctx.guild
+    existing_channel = discord.utils.get(guild.channels, name=channel_name)
+    if not existing_channel:
+        print(f'Creating a new channel: {channel_name}')
+        await guild.create_text_channel(channel_name)
+
+@bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+@bot.command(name='roll_dice', help='Simulates rolling dice.')
+async def roll(ctx, number_of_dice: int, number_of_sides: int):
+    dice = [
+        str(random.choice(range(1, number_of_sides + 1)))
+        for _ in range(number_of_dice)
+    ]
+    await ctx.send(', '.join(dice))
+
+bot.run(TOKEN)
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Discord Bot
+* `mv e.env .env`
+* get a discord auth token
+
+## Source
+https://realpython.com/how-to-make-a-discord-bot-python
